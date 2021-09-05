09632890c1be2368eca6eb7f54f65500d5dfcebe
kalebthe1st <kalebmoges16@gmail.com>
2021-09-05 11:42:04 -0700

    uu

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("%d",size(int));
+size(char);
+size(int);
+size(long int);
+size(long long int);
+size(float);
+return (0);
+}
