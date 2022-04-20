65f330e1420922921c133799d619bf6b6ebe0487
kaleb <kalebmoges16@gmail.com>
2022-04-20 14:44:41 -0700

    done an advanced task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int randomizer = 0;
+	int N = 10;
+	srand((unsigned int)(time(NULL)));
+    char numbers[] = "0123456789";
+    char letter[] = "abcdefghijklmnoqprstuvwyzx";
+    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+    char symbols[] = "!@#$^&*?";
+    char password[N];
+    randomizer = rand() % 4;
+ for (i = 0; i < N; i++) {
+  
+        if (randomizer == 1) {
+            password[i] = numbers[rand() % 10];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 2) {
+            password[i] = symbols[rand() % 8];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 3) {
+            password[i] = LETTER[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else {
+            password[i] = letter[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+    }
+printf("\n");
+ return(0); 
+}
